1ac7a3f14b6dc28cc9acbb6d1f9d820a01cbd3c0
MartyIX <203266+MartyIX@users.noreply.github.com>
2023-11-10 17:23:11 +0100

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace VisualElementXY202311;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace VisualElementXY202311;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
